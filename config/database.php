ef20723ced60d090cab34c2f7a5a4cd08f0d55b0
Honore Hounwanou <mercuryseries@gmail.com>
2015-06-12 13:26:58 +0000

    Fix database config issue

--- config/database.php
+++ config/database.php
@@ -66,10 +66,10 @@ return [
 
         'pgsql' => [
             'driver'   => 'pgsql',
-            'host'     => parse_url(getenv("DATABASE_URL"))["host"],
-            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
-            'username' => parse_url(getenv("DATABASE_URL"))["user"],
-            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+            'host'     => @parse_url(getenv("DATABASE_URL"))["host"],
+            'database' => @substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+            'username' => @parse_url(getenv("DATABASE_URL"))["user"],
+            'password' => @parse_url(getenv("DATABASE_URL"))["pass"],
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
